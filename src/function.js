f957fe49c49a3dd664c3b7525f01f670867a3699
Jarosław Zagłoba <jaroslawzagloba@gmail.com>
2021-01-05 20:41:21 +0100

    implementacja funkcji getById, update i delete

--- src/function.js
+++ src/function.js
@@ -1,10 +1,10 @@
 module.exports.getAll = async event => {
   console.log('getAll');
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event)
-  };
+  return returnOnSuccess({
+    functionName: 'getAll',
+    event
+  })
 }
 
 module.exports.create = async event => {
@@ -13,8 +13,57 @@ module.exports.create = async event => {
   const body = JSON.parse(event.body)
   console.log(body);
 
+  return returnOnSuccess({
+    functionName: 'create',
+    body,
+    event
+  })
+}
+
+module.exports.getById = async event => {
+  console.log('getById');
+
+  const itemId = event.pathParameters.id
+  console.log(`itemId=${itemId}`);
+
+  return returnOnSuccess({
+    functionName: 'getById',
+    itemId,
+    event
+  })
+}
+
+module.exports.update = async event => {
+  console.log('update');
+
+  const itemId = event.pathParameters.id
+  const body = JSON.parse(event.body)
+  console.log(`itemId=${itemId}`)
+
+  return returnOnSuccess({
+    functionName: 'update',
+    itemId,
+    body,
+    event
+  })
+}
+
+module.exports.delete = async event => {
+  console.log('delete');
+
+  const itemId = event.pathParameters.id
+  console.log(`itemId=${itemId}`);
+
+  return returnOnSuccess({
+    functionName: 'delete',
+    itemId,
+    event
+  })
+}
+
+const returnOnSuccess = body => {
   return {
     statusCode: 200,
-    body: JSON.stringify(event)
-  };
+    body: JSON.stringify(body)
+  }
 }
