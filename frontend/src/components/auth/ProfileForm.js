a4e511bc898118c8155d45791d135a385769b1d1
Jarosław Zagłoba <jaroslawzagloba@gmail.com>
2021-02-05 21:20:41 +0100

    dodanie logiki aktualizacji profilu

--- frontend/src/components/auth/ProfileForm.js
+++ frontend/src/components/auth/ProfileForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Form, Grid, Header, Image, Segment, Message } from 'semantic-ui-react'
 import logo from '../../logo.png'
 import { Context } from "../../Store";
+import { Auth } from 'aws-amplify'
 
 const ProfileForm = () => {
 
@@ -12,18 +13,19 @@ const ProfileForm = () => {
       family_name: state.lastname.trim(),
       email: state.email.trim()
     }
-    
-    // cognito code goes here
-
+    const user = await Auth.currentAuthenticatedUser()
+    await Auth.updateUserAttributes(user, newAttributes) // faktyczna modyfikacja profilu
     dispatch({
       type: "login",
       attributes: {
-        given_name: 'Paweł',
-        family_name: 'Zubkiewicz'
+        given_name: state.firstname,
+        family_name: state.lastname
       }
     })
     setState({ ...state, updated: true })
   }
+
+  // END COGNITO UPDATE
 
   const { dispatch } = React.useContext(Context);
   const [state, setState] = React.useState({ error: null, updated: null, firstname: '', lastname: '', email: '' })
@@ -34,7 +36,33 @@ const ProfileForm = () => {
     setState(newState)
   }
 
+  const [result, loading] = useAsyncHook();
+  function useAsyncHook() {
+    const [result, setResult] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
 
+    React.useEffect(() => {
+      async function fetchUserData() {
+        const user = await Auth.currentAuthenticatedUser()
+        const atts = await Auth.userAttributes(user)
+        // console.log(user)
+        // console.log(atts)
+        const attributes = {}
+        atts.forEach(e => attributes[e.Name] = e.Value)
+        console.log('attributes', attributes);
+        setLoading(false)
+        setState({
+          error: null,
+          updated: null,
+          firstname: attributes.given_name ? attributes.given_name : '',
+          lastname: attributes.family_name ? attributes.family_name : '',
+          email: attributes.email ? attributes.email : ''
+        })
+      }
+      fetchUserData()
+    }, [''])
+    return [result, loading]
+  }
 
   return (
     <Grid textAlign='center' style={{ height: '60vh' }} verticalAlign='middle'>
@@ -46,20 +74,20 @@ const ProfileForm = () => {
           <Message info header='Profil zaktualizowany w Amazon Cognito' />
         }
 
-        <Form size='large' loading={false}>
+        <Form size='large' loading={loading}>
           <Segment stacked>
             <Form.Input fluid icon='user' iconPosition='left' placeholder='First name'
               name='firstname'
               onChange={handleChange}
-              defaultValue='' />
+              defaultValue={state.firstname} />
             <Form.Input fluid icon='user' iconPosition='left' placeholder='Last name'
               name='lastname'
               onChange={handleChange}
-              defaultValue='' />
+              defaultValue={state.lastname} />
             <Form.Input fluid icon='mail' iconPosition='left' placeholder='E-mail address'
               name='email'
               onChange={handleChange}
-              required defaultValue='' />
+              required defaultValue={state.email} />
             <Button fluid size='large' onClick={updateProfile}>Zmień profil</Button>
           </Segment>
         </Form>
