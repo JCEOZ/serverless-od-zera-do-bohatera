7a2765b5eb1308ad17da91ed1471e37080c54e29
Jarosław Zagłoba <jaroslawzagloba@gmail.com>
2021-02-15 21:54:19 +0100

    poprawa formatowania

--- backend/import-file/__test__/processFile/function.test.js
+++ backend/import-file/__test__/processFile/function.test.js
@@ -8,24 +8,24 @@ const lambdaRewired = rewire('../../src/processFile/function')
 
 describe('Process File', () => {
     it('should process business logic', async () => {
-         
-       // GIVEN
-       const businessLogic = lambdaRewired.__get__('businessLogic')
-       const fileInfo = {
-           bucketName: "bucket",
-           fileName: "file.csv"
-       }
 
-       const getFileMock = (fileInfo) => {
-           const { bucketName, fileName } = fileInfo
+        // GIVEN
+        const businessLogic = lambdaRewired.__get__('businessLogic')
+        const fileInfo = {
+            bucketName: "bucket",
+            fileName: "file.csv"
+        }
+
+        const getFileMock = (fileInfo) => {
+            const { bucketName, fileName } = fileInfo
             expect(bucketName).toBe('bucket')
             expect(fileName).toBe('file.csv')
-       }
+        }
 
-       // WHEN
-       const actual = await businessLogic(fileInfo, getFile)
+        // WHEN
+        const actual = await businessLogic(fileInfo, getFile)
 
-       //THEN
-       expect(businessLogic).toBeTruthy()
+        //THEN
+        expect(businessLogic).toBeTruthy()
     });
 });
