0d0061fee263a71746ebca9bad91ea80bd3f8553
Jarosław Zagłoba <jaroslawzagloba@gmail.com>
2021-02-15 21:53:46 +0100

    walidacja parametrów wejściowych w mocku

--- backend/import-file/__test__/processFile/function.test.js
+++ backend/import-file/__test__/processFile/function.test.js
@@ -6,13 +6,24 @@ const rewire = require('rewire');
 
 const lambdaRewired = rewire('../../src/processFile/function')
 
-describe('Process File', async () => {
-    it('should process business logic', () => {
+describe('Process File', () => {
+    it('should process business logic', async () => {
          
        // GIVEN
        const businessLogic = lambdaRewired.__get__('businessLogic')
+       const fileInfo = {
+           bucketName: "bucket",
+           fileName: "file.csv"
+       }
+
+       const getFileMock = (fileInfo) => {
+           const { bucketName, fileName } = fileInfo
+            expect(bucketName).toBe('bucket')
+            expect(fileName).toBe('file.csv')
+       }
 
        // WHEN
+       const actual = await businessLogic(fileInfo, getFile)
 
        //THEN
        expect(businessLogic).toBeTruthy()
